c2133d6d7994078d932ada21eeefcfec1dcd2d68
romanovmaksim <massib796@gmail.com>
2023-11-21 17:08:23 +0300

    переписана команда /start

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/telebot.v3 v3.2.1 // indirect
 )
--- internal/botBase/botCommands/botCommands.go
+++ internal/botBase/botCommands/botCommands.go
@@ -2,51 +2,110 @@ package botCommands
 
 import (
 	"fmt"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"play_portal_bot/internal/botBase/helpingMethods"
-	"play_portal_bot/internal/loggers"
+	"gopkg.in/telebot.v3"
 	"play_portal_bot/pkg/utils/structures"
 )
 
 // BotStart КОМАНДА СТАРТА, ПО СОВМЕСТИТЕЛЬСТВУ ВЫВОД ГЛАВНОГО МЕНЮ
-func BotStart(bot *tgbotapi.BotAPI, update *tgbotapi.Update) {
+// func Start(bot *tgbotapi.BotAPI, update *tgbotapi.Update) {
+//
+//		// =========PARAMS=========
+//		chatID := update.Message.Chat.ID
+//		picPath := "pkg/utils/data/img/pngtree-isolated-cat-on-white-background-png-image_7094927.png"
+//		messageContent := "МАГАЗИН ГИДРА"
+//		messageData := &structures.MessageData{
+//			MessageID:   update.Message.MessageID,
+//			ChatID:      update.Message.Chat.ID,
+//			Command:     "mainMenu",
+//			PrevCommand: "",
+//		}
+//		fmt.Println(messageData)
+//		positions := []int{2, 2}
+//		commands := &[]structures.Command{
+//			{Text: "Магазин", Command: "showShop"},
+//			{Text: "Кабинет", Command: "showPersonalArea"},
+//			{Text: "Поддержка", Command: "showSupport"},
+//			{Text: "FAQ", Command: "showFAQ"},
+//		}
+//		// =========PARAMS=========
+//
+//		msg := helpingMethods.CreateMessage(chatID, picPath, messageContent, commands, messageData, positions)
+//		message, err := bot.Send(msg)
+//		if err != nil {
+//			loggers.ErrorLogger.Println(err.Error())
+//		}
+//		fmt.Println(message.MessageID, "MESSAGEID SHOP")
+//		newKB := helpingMethods.CreateInline(&structures.MessageData{
+//			MessageID:   message.MessageID,
+//			ChatID:      message.Chat.ID,
+//			Command:     "mainMenu",
+//			PrevCommand: "",
+//		}, []int{2, 2}, *commands...)
+//		newKBConf := tgbotapi.NewEditMessageReplyMarkup(chatID, message.MessageID, *newKB)
+//		_, err = bot.Send(newKBConf)
+//		if err != nil {
+//			fmt.Println(messageData)
+//			loggers.ErrorLogger.Println(err.Error(), "editKBError")
+//		}
+//	}
+func Start(c telebot.Context) error {
 
 	// =========PARAMS=========
-	chatID := update.Message.Chat.ID
+	chatID := c.Chat().ID
 	picPath := "pkg/utils/data/img/pngtree-isolated-cat-on-white-background-png-image_7094927.png"
 	messageContent := "МАГАЗИН ГИДРА"
 	messageData := &structures.MessageData{
-		MessageID:   update.Message.MessageID,
-		ChatID:      update.Message.Chat.ID,
+		MessageID:   c.Message().ID,
+		ChatID:      c.Chat().ID,
 		Command:     "mainMenu",
 		PrevCommand: "",
 	}
 	fmt.Println(messageData)
-	positions := []int{2, 2}
-	commands := &[]structures.Command{
-		{Text: "Магазин", Command: "showShop"},
-		{Text: "Кабинет", Command: "showPersonalArea"},
-		{Text: "Поддержка", Command: "showSupport"},
-		{Text: "FAQ", Command: "showFAQ"},
-	}
+	//positions := []int{2, 2}
+	//commands := &[]structures.Command{
+	//	{Text: "Магазин", Command: "showShop"},
+	//	{Text: "Кабинет", Command: "showPersonalArea"},
+	//	{Text: "Поддержка", Command: "showSupport"},
+	//	{Text: "FAQ", Command: "showFAQ"},
+	//}
 	// =========PARAMS=========
 
-	msg := helpingMethods.CreateMessage(chatID, picPath, messageContent, commands, messageData, positions)
-	message, err := bot.Send(msg)
-	if err != nil {
-		loggers.ErrorLogger.Println(err.Error())
+	//msg := helpingMethods.CreateMessage(chatID, picPath, messageContent, commands, messageData, positions)
+	msg := &telebot.Photo{
+		File:    telebot.FromDisk(picPath),
+		Caption: messageContent,
 	}
-	fmt.Println(message.MessageID, "MESSAGEID SHOP")
-	newKB := helpingMethods.CreateInline(&structures.MessageData{
-		MessageID:   message.MessageID,
-		ChatID:      message.Chat.ID,
-		Command:     "mainMenu",
-		PrevCommand: "",
-	}, []int{2, 2}, *commands...)
-	newKBConf := tgbotapi.NewEditMessageReplyMarkup(chatID, message.MessageID, *newKB)
-	_, err = bot.Send(newKBConf)
-	if err != nil {
-		fmt.Println(messageData)
-		loggers.ErrorLogger.Println(err.Error(), "editKBError")
+	keys := [][]telebot.InlineButton{
+		{
+			{
+				Text: "Button 1",
+			},
+			{
+				Text: "Button 2",
+			},
+		},
 	}
+
+	keyboard := &telebot.ReplyMarkup{
+		InlineKeyboard: keys,
+	}
+	fmt.Println(msg)
+	_, err := c.Bot().Send(telebot.ChatID(chatID), msg, &telebot.SendOptions{
+		ParseMode:   telebot.ModeHTML,
+		ReplyMarkup: keyboard})
+	return err
+	//
+	//fmt.Println(message.MessageID, "MESSAGEID SHOP")
+	//newKB := helpingMethods.CreateInline(&structures.MessageData{
+	//	MessageID:   message.MessageID,
+	//	ChatID:      message.Chat.ID,
+	//	Command:     "mainMenu",
+	//	PrevCommand: "",
+	//}, []int{2, 2}, *commands...)
+	//newKBConf := tgbotapi.NewEditMessageReplyMarkup(chatID, message.MessageID, *newKB)
+	//_, err = bot.Send(newKBConf)
+	//if err != nil {
+	//	fmt.Println(messageData)
+	//	loggers.ErrorLogger.Println(err.Error(), "editKBError")
+	//}
 }
